7642dfd1591a20dd753ecce3798b0780b6560cae
sshtel <sshtel@gmail.com>
2015-07-20 22:10:51 +0900

    OpenCL basic sample update

--- sample/sample_vs2013/sample_vs2013/main.cpp
+++ sample/sample_vs2013/sample_vs2013/main.cpp
@@ -2,13 +2,16 @@
 #include <opencv2/opencv.hpp>
 #include <device_ocl.h>
 
+using namespace cv;
+using namespace std;
+
+
+#define IMAGE_ROOT "..\\..\\..\\images"
+#define HOG_IMG IMAGE_ROOT"\\hog_1.jpg"
+#define HOG_VID IMAGE_ROOT"\\pedestrian.mp4"
+
 int main(){
 
-	cv::String vendorName;
-	int deviceType;
-	int vendor;
-	bool clDeviceFound = false;
-	
 	/*
 	set environment variables before run this program.
 	OpenCV 3.0 does not provide cv::ocl::setDevice() API
@@ -19,6 +22,19 @@ int main(){
 
 	*/
 
+	cv::String vendorName;
+	int deviceType;
+	int vendor;
+	bool clDeviceFound = false;
+
+	cv::ocl::Device device = cv::ocl::Device::getDefault();
+	vendorName = device.vendorName();
+	deviceType = device.type();
+	std::cout << "Vendor : " << vendorName.c_str() << std::endl;
+	std::cout << "type  : " << deviceType << std::endl;
+
+
+
 	std::string platformName = "AMD";
 	//std::string platformName = "Intel";
 	deviceType = cv::ocl::Device::TYPE_GPU;
@@ -28,5 +44,7 @@ int main(){
 	if (devOcl.checkDefaultDevice(vendor, deviceType)) {
 		std::cout << " check device OK" << std::endl;
 	}
+
+
 	return 0;
 }
