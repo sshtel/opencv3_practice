cffad6cc577de51e620be553fc4e5dfac775f08e
sshtel <sshtel@gmail.com>
2015-08-30 21:05:46 +0900

    add tools/common_directory.hpp

---
+++ sample/tools/common_directory.hpp
@@ -0,0 +1,15 @@
+#ifndef __COMMON_DIRECTORY_H
+#define __COMMON_DIRECTORY_H
+
+#include <stdio.h>  /* defines FILENAME_MAX */
+#ifdef _WIN32
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+
+
+
+#endif
